79fdf9833cc98d5bc5d07964ba778953a4d2189d
aseelr7 <aseelr95@gmail.com>
2024-08-07 21:44:51 -0400

    Secondary Push

--- src/Test/SecondClass.java
+++ src/Test/SecondClass.java
@@ -1,11 +1,13 @@
 package Test;
 
+import java.sql.SQLOutput;
+
 public class SecondClass {
 
     public void second() {
 
         System.out.println("hello!");
-
+        System.out.println("Yo!");
 
     }
 }
